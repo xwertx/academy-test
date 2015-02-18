1f8b605fccbbe04130232abf179a5ab7a4561e02
xwertx <rmadejowicz@tlen.pl>
2015-02-18 11:28:23 +0100

    set route users page (/users) to /all_users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
-  match '/users', :to => 'users#index', :as => "all_users", :via => "get"
+  get '/all_users', to: 'users#index'
   get '/cities' => 'cities#index'
 end
